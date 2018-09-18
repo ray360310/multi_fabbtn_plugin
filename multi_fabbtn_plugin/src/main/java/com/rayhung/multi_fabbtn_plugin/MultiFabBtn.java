d1681f462e0f9b7f14efcbfb65fc32b93ab255e5
洪明宏 <>
2018-09-18 11:33:00 +0800

    add textview on fab, can change color,size and text.

--- multi_fabbtn_plugin/src/main/java/com/rayhung/multi_fabbtn_plugin/MultiFabBtn.java
+++ multi_fabbtn_plugin/src/main/java/com/rayhung/multi_fabbtn_plugin/MultiFabBtn.java
@@ -10,6 +10,8 @@ import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MultiFabBtn extends ConstraintLayout implements MovableFabCallback{
@@ -20,12 +22,22 @@ public class MultiFabBtn extends ConstraintLayout implements MovableFabCallback{
     private MultiFabBtnCallback mCallback;
 
     private ConstraintLayout mainClayout;
+    private FrameLayout fab01FLayout;
+    private FrameLayout fab02FLayout;
+    private FrameLayout fab03FLayout;
+    private FrameLayout fab04FLayout;
+    private FrameLayout fab05FLayout;
     private MovableFab mainFab;
     private FloatingActionButton fab01;
     private FloatingActionButton fab02;
     private FloatingActionButton fab03;
     private FloatingActionButton fab04;
     private FloatingActionButton fab05;
+    private TextView fab01Txw;
+    private TextView fab02Txw;
+    private TextView fab03Txw;
+    private TextView fab04Txw;
+    private TextView fab05Txw;
 
     private int viewHeight;
     private int viewWidth;
@@ -75,11 +87,22 @@ public class MultiFabBtn extends ConstraintLayout implements MovableFabCallback{
         mView = LayoutInflater.from(mContext).inflate(R.layout.multifabbtn,this,true);
         mainClayout = mView.findViewById(R.id.mainClayout);
         mainFab = mView.findViewById(R.id.fab_main);
+        fab01FLayout = mView.findViewById(R.id.fab01flayout);
+        fab02FLayout = mView.findViewById(R.id.fab02flayout);
+        fab03FLayout = mView.findViewById(R.id.fab03flayout);
+        fab04FLayout = mView.findViewById(R.id.fab04flayout);
+        fab05FLayout = mView.findViewById(R.id.fab05flayout);
         fab01 = mView.findViewById(R.id.fab_01);
         fab02 = mView.findViewById(R.id.fab_02);
         fab03 = mView.findViewById(R.id.fab_03);
         fab04 = mView.findViewById(R.id.fab_04);
         fab05 = mView.findViewById(R.id.fab_05);
+        fab01Txw = mView.findViewById(R.id.fab01txw);
+        fab02Txw = mView.findViewById(R.id.fab02txw);
+        fab03Txw = mView.findViewById(R.id.fab03txw);
+        fab04Txw = mView.findViewById(R.id.fab04txw);
+        fab05Txw = mView.findViewById(R.id.fab05txw);
+
         mainFab.setCallbackListener(this);
         onClickHandler();
     }
@@ -172,149 +195,125 @@ public class MultiFabBtn extends ConstraintLayout implements MovableFabCallback{
     }
 
     private void createIQuadrantButtons(float x, float y){
-        if (fab01.getVisibility() == View.GONE) {
+        if (fab01FLayout.getVisibility() == View.GONE) {
             TransitionManager.beginDelayedTransition(mainClayout);
-            fab01.show();
-            fab02.show();
-            fab03.show();
-            fab04.show();
-            fab05.show();
+            fab01FLayout.setVisibility(VISIBLE);
+            fab02FLayout.setVisibility(VISIBLE);
+            fab03FLayout.setVisibility(VISIBLE);
+            fab04FLayout.setVisibility(VISIBLE);
+            fab05FLayout.setVisibility(VISIBLE);
             ConstraintLayout.LayoutParams cLp = new ConstraintLayout.LayoutParams(multiFabBtnWidth, multiFabBtnWidth);
-            fab01.setLayoutParams(cLp);
-            fab02.setLayoutParams(cLp);
-            fab03.setLayoutParams(cLp);
-            fab04.setLayoutParams(cLp);
-            fab05.setLayoutParams(cLp);
+            fab01FLayout.setLayoutParams(cLp);
+            fab02FLayout.setLayoutParams(cLp);
+            fab03FLayout.setLayoutParams(cLp);
+            fab04FLayout.setLayoutParams(cLp);
+            fab05FLayout.setLayoutParams(cLp);
         }
-        fab01.setX(x- fabBtnDistance);
-        fab01.setY(y);
-        fab02.setX(x-degree30x);
-        fab02.setY(y+degree30y);
-        fab03.setX(x-degree45x);
-        fab03.setY(y+degree45y);
-        fab04.setX(x-degree60x);
-        fab04.setY(y+degree60y);
-        fab05.setX(x);
-        fab05.setY(y+ fabBtnDistance);
+        fab01FLayout.setX(x- fabBtnDistance);
+        fab01FLayout.setY(y);
+        fab02FLayout.setX(x-degree30x);
+        fab02FLayout.setY(y+degree30y);
+        fab03FLayout.setX(x-degree45x);
+        fab03FLayout.setY(y+degree45y);
+        fab04FLayout.setX(x-degree60x);
+        fab04FLayout.setY(y+degree60y);
+        fab05FLayout.setX(x);
+        fab05FLayout.setY(y+ fabBtnDistance);
         isFabOpen = true;
     }
 
     private void createIIQuadrantButtons(float x, float y){
-        if (fab01.getVisibility() == View.GONE) {
+        if (fab01FLayout.getVisibility() == View.GONE) {
             TransitionManager.beginDelayedTransition(mainClayout);
-            fab01.show();
-            fab02.show();
-            fab03.show();
-            fab04.show();
-            fab05.show();
+            fab01FLayout.setVisibility(VISIBLE);
+            fab02FLayout.setVisibility(VISIBLE);
+            fab03FLayout.setVisibility(VISIBLE);
+            fab04FLayout.setVisibility(VISIBLE);
+            fab05FLayout.setVisibility(VISIBLE);
             ConstraintLayout.LayoutParams cLp = new ConstraintLayout.LayoutParams(multiFabBtnWidth, multiFabBtnWidth);
-            fab01.setLayoutParams(cLp);
-            fab02.setLayoutParams(cLp);
-            fab03.setLayoutParams(cLp);
-            fab04.setLayoutParams(cLp);
-            fab05.setLayoutParams(cLp);
+            fab01FLayout.setLayoutParams(cLp);
+            fab02FLayout.setLayoutParams(cLp);
+            fab03FLayout.setLayoutParams(cLp);
+            fab04FLayout.setLayoutParams(cLp);
+            fab05FLayout.setLayoutParams(cLp);
         }
-        fab01.setX(x+ fabBtnDistance);
-        fab01.setY(y);
-        fab02.setX(x+degree30x);
-        fab02.setY(y+degree30y);
-        fab03.setX(x+degree45x);
-        fab03.setY(y+degree45y);
-        fab04.setX(x+degree60x);
-        fab04.setY(y+degree60y);
-        fab05.setX(x);
-        fab05.setY(y+ fabBtnDistance);
+        fab01FLayout.setX(x+ fabBtnDistance);
+        fab01FLayout.setY(y);
+        fab02FLayout.setX(x+degree30x);
+        fab02FLayout.setY(y+degree30y);
+        fab03FLayout.setX(x+degree45x);
+        fab03FLayout.setY(y+degree45y);
+        fab04FLayout.setX(x+degree60x);
+        fab04FLayout.setY(y+degree60y);
+        fab05FLayout.setX(x);
+        fab05FLayout.setY(y+ fabBtnDistance);
         isFabOpen = true;
     }
 
     private void createIIIQuadrantButtons(float x, float y){
-        if (fab01.getVisibility() == View.GONE) {
+        if (fab01FLayout.getVisibility() == View.GONE) {
             TransitionManager.beginDelayedTransition(mainClayout);
-            fab01.show();
-            fab02.show();
-            fab03.show();
-            fab04.show();
-            fab05.show();
+            fab01FLayout.setVisibility(VISIBLE);
+            fab02FLayout.setVisibility(VISIBLE);
+            fab03FLayout.setVisibility(VISIBLE);
+            fab04FLayout.setVisibility(VISIBLE);
+            fab05FLayout.setVisibility(VISIBLE);
             ConstraintLayout.LayoutParams cLp = new ConstraintLayout.LayoutParams(multiFabBtnWidth, multiFabBtnWidth);
-            fab01.setLayoutParams(cLp);
-            fab02.setLayoutParams(cLp);
-            fab03.setLayoutParams(cLp);
-            fab04.setLayoutParams(cLp);
-            fab05.setLayoutParams(cLp);
+            fab01FLayout.setLayoutParams(cLp);
+            fab02FLayout.setLayoutParams(cLp);
+            fab03FLayout.setLayoutParams(cLp);
+            fab04FLayout.setLayoutParams(cLp);
+            fab05FLayout.setLayoutParams(cLp);
         }
-        fab01.setX(x);
-        fab01.setY(y- fabBtnDistance);
-        fab02.setX(x+degree60x);
-        fab02.setY(y-degree60y);
-        fab03.setX(x+degree45x);
-        fab03.setY(y-degree45y);
-        fab04.setX(x+degree30x);
-        fab04.setY(y-degree30y);
-        fab05.setX(x+ fabBtnDistance);
-        fab05.setY(y);
+        fab01FLayout.setX(x);
+        fab01FLayout.setY(y- fabBtnDistance);
+        fab02FLayout.setX(x+degree60x);
+        fab02FLayout.setY(y-degree60y);
+        fab03FLayout.setX(x+degree45x);
+        fab03FLayout.setY(y-degree45y);
+        fab04FLayout.setX(x+degree30x);
+        fab04FLayout.setY(y-degree30y);
+        fab05FLayout.setX(x+ fabBtnDistance);
+        fab05FLayout.setY(y);
         isFabOpen = true;
     }
 
     private void createIVQuadrantButtons(float x, float y){
-        if (fab01.getVisibility() == View.GONE) {
+        if (fab01FLayout.getVisibility() == View.GONE) {
             TransitionManager.beginDelayedTransition(mainClayout);
-            fab01.show();
-            fab02.show();
-            fab03.show();
-            fab04.show();
-            fab05.show();
+            fab01FLayout.setVisibility(VISIBLE);
+            fab02FLayout.setVisibility(VISIBLE);
+            fab03FLayout.setVisibility(VISIBLE);
+            fab04FLayout.setVisibility(VISIBLE);
+            fab05FLayout.setVisibility(VISIBLE);
             ConstraintLayout.LayoutParams cLp = new ConstraintLayout.LayoutParams(multiFabBtnWidth, multiFabBtnWidth);
-            fab01.setLayoutParams(cLp);
-            fab02.setLayoutParams(cLp);
-            fab03.setLayoutParams(cLp);
-            fab04.setLayoutParams(cLp);
-            fab05.setLayoutParams(cLp);
+            fab01FLayout.setLayoutParams(cLp);
+            fab02FLayout.setLayoutParams(cLp);
+            fab03FLayout.setLayoutParams(cLp);
+            fab04FLayout.setLayoutParams(cLp);
+            fab05FLayout.setLayoutParams(cLp);
         }
-        fab01.setX(x);
-        fab01.setY(y- fabBtnDistance);
-        fab02.setX(x-degree60x);
-        fab02.setY(y-degree60y);
-        fab03.setX(x-degree45x);
-        fab03.setY(y-degree45y);
-        fab04.setX(x-degree30x);
-        fab04.setY(y-degree30y);
-        fab05.setX(x- fabBtnDistance);
-        fab05.setY(y);
+        fab01FLayout.setX(x);
+        fab01FLayout.setY(y- fabBtnDistance);
+        fab02FLayout.setX(x-degree60x);
+        fab02FLayout.setY(y-degree60y);
+        fab03FLayout.setX(x-degree45x);
+        fab03FLayout.setY(y-degree45y);
+        fab04FLayout.setX(x-degree30x);
+        fab04FLayout.setY(y-degree30y);
+        fab05FLayout.setX(x- fabBtnDistance);
+        fab05FLayout.setY(y);
         isFabOpen = true;
     }
 
     private void closeFABMenu(){
 //        TransitionManager.beginDelayedTransition(mainClayout);
-        if (fab01 != null) fab01.hide();
-        if (fab02 != null) fab02.hide();
-        if (fab03 != null) fab03.hide();
-        if (fab04 != null) fab04.hide();
-        if (fab05 != null) fab05.hide();
+        if (fab01FLayout != null) fab01FLayout.setVisibility(GONE);
+        if (fab02FLayout != null) fab02FLayout.setVisibility(GONE);
+        if (fab03FLayout != null) fab03FLayout.setVisibility(GONE);
+        if (fab04FLayout != null) fab04FLayout.setVisibility(GONE);
+        if (fab05FLayout != null) fab05FLayout.setVisibility(GONE);
         isFabOpen = false;
-    }
-
-    public void setMainFabIcon(int ResId){
-        mainFab.setImageResource(ResId);
-    }
-
-    public void setFab01Icon(int ResId){
-        fab01.setImageResource(ResId);
-    }
-
-    public void setFab02Icon(int ResId){
-        fab02.setImageResource(ResId);
-    }
-
-    public void setFab03Icon(int ResId){
-        fab03.setImageResource(ResId);
-    }
-
-    public void setFab04Icon(int ResId){
-        fab04.setImageResource(ResId);
-    }
-
-    public void setFab05Icon(int ResId){
-        fab05.setImageResource(ResId);
     }
 
     /**
@@ -332,6 +331,22 @@ public class MultiFabBtn extends ConstraintLayout implements MovableFabCallback{
         mainFab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(ResId)));
     }
 
+    public void setFabsTextSize(float textSizeSP){
+        fab01Txw.setTextSize(TypedValue.COMPLEX_UNIT_SP,textSizeSP);
+        fab02Txw.setTextSize(TypedValue.COMPLEX_UNIT_SP,textSizeSP);
+        fab03Txw.setTextSize(TypedValue.COMPLEX_UNIT_SP,textSizeSP);
+        fab04Txw.setTextSize(TypedValue.COMPLEX_UNIT_SP,textSizeSP);
+        fab05Txw.setTextSize(TypedValue.COMPLEX_UNIT_SP,textSizeSP);
+    }
+
+    public void setFabsTextColor(int ResID){
+        fab01Txw.setTextColor(ResID);
+        fab02Txw.setTextColor(ResID);
+        fab03Txw.setTextColor(ResID);
+        fab04Txw.setTextColor(ResID);
+        fab05Txw.setTextColor(ResID);
+    }
+
     /**
      * Fab01
      * */
@@ -345,6 +360,14 @@ public class MultiFabBtn extends ConstraintLayout implements MovableFabCallback{
 
     public void setFab01Background(int ResId){
         fab01.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(ResId)));
+    }
+
+    public void setFab01Text(String inputStr){
+        fab01Txw.setText(inputStr);
+    }
+
+    public void setFab01TextColor(int ResID){
+        fab01Txw.setTextColor(getResources().getColor(ResID));
     }
 
     /**
@@ -362,6 +385,14 @@ public class MultiFabBtn extends ConstraintLayout implements MovableFabCallback{
         fab02.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(ResId)));
     }
 
+    public void setFab02Text(String inputStr){
+        fab02Txw.setText(inputStr);
+    }
+
+    public void setFab02TextColor(int ResID){
+        fab02Txw.setTextColor(getResources().getColor(ResID));
+    }
+
     /**
      * Fab03
      * */
@@ -375,6 +406,14 @@ public class MultiFabBtn extends ConstraintLayout implements MovableFabCallback{
 
     public void setFab03Background(int ResId){
         fab03.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(ResId)));
+    }
+
+    public void setFab03Text(String inputStr){
+        fab03Txw.setText(inputStr);
+    }
+
+    public void setFab03TextColor(int ResID){
+        fab03Txw.setTextColor(getResources().getColor(ResID));
     }
 
     /**
@@ -392,6 +431,14 @@ public class MultiFabBtn extends ConstraintLayout implements MovableFabCallback{
         fab04.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(ResId)));
     }
 
+    public void setFab04Text(String inputStr){
+        fab04Txw.setText(inputStr);
+    }
+
+    public void setFab04TextColor(int ResID){
+        fab04Txw.setTextColor(getResources().getColor(ResID));
+    }
+
     /**
      * Fab05
      * */
@@ -405,6 +452,14 @@ public class MultiFabBtn extends ConstraintLayout implements MovableFabCallback{
 
     public void setFab05Background(int ResId){
         fab05.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(ResId)));
+    }
+
+    public void setFab05Text(String inputStr){
+        fab05Txw.setText(inputStr);
+    }
+
+    public void setFab05TextColor(int ResID){
+        fab05Txw.setTextColor(getResources().getColor(ResID));
     }
 
 }
